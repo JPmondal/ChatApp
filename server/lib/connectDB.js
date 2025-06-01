165d60b4eb2d296c4e5185d96d3fa3bf3803c10d
JP Mondal <jpmondal2003@gmail.com>
2025-06-01 23:16:12 +0600

    fix: refactor connectDB function for improved MongoDB connection handling

--- server/lib/connectDB.js
+++ server/lib/connectDB.js
@@ -1,18 +1,38 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () =>{
+//     try {
+
+//          mongoose.connection.on('connected',()=>{
+//             console.log("Connected to MongoDB Succesfully")
+//         })
+
+//         await mongoose.connect(`${process.env.MONGODB_URI}/chatapp`)
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// export default connectDB
+
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-    try {
+let isConnected = false;
 
-         mongoose.connection.on('connected',()=>{
-            console.log("Connected to MongoDB Succesfully")
-        })
-        
-        await mongoose.connect(`${process.env.MONGODB_URI}/chatapp`)
-       
+const connectDB = async () => {
+  if (isConnected) return;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+    throw new Error("MongoDB connection failed");
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export default connectDB
+export default connectDB;
